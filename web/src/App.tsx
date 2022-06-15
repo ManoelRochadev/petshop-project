95d47a255035588b0394af0c179a0f09be5550f1
Manoel Rocha <manoelmelo367@gmail.com>
2022-06-15 15:47:57 +0000

    BUG routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,417 @@
-import { Routes, Route } from "react-router-dom";
-import { Dashboard } from "./components/Dashboard";
-import { CreateUser } from "./components/CreateUser";
-import { CreateGroomer } from "./components/CreateGroomer";
-import { CreateDoctor } from "./components/CreateDoctor";
-import { Home } from "./components/Home";
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+  Img,
+  Heading,
+  Container,
+  createIcon,
+  SimpleGrid,
+  chakra,
+  VisuallyHidden,
+} from '@chakra-ui/react';
+import {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+} from '@chakra-ui/icons';
+import { useNavigate, Link as LinkRouter } from 'react-router-dom';
+import { ReactNode } from 'react';
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import imageLogo from '../assets/ifaro.png'
 
 export default function App() {
+  const { isOpen, onToggle } = useDisclosure();
+
+  const navigate = useNavigate()
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/register/user" element={<CreateUser/>}/>
-      <Route path='/register/groomer' element={<CreateGroomer />} />
-      <Route path='/register/doctor' element={<CreateDoctor />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-    </Routes>
-  )
+    <>
+      <Box display='block'>
+        <Flex
+          bg={useColorModeValue('white', 'gray.800')}
+          color={useColorModeValue('gray.600', 'white')}
+          minH={'60px'}
+          py={{ base: 2 }}
+          px={{ base: 4 }}
+          borderBottom={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.900')}
+          align={'center'}>
+          <Flex
+            flex={{ base: 1, md: 'auto' }}
+            ml={{ base: -2 }}
+            display={{ base: 'flex', md: 'none' }}>
+            <IconButton
+              onClick={onToggle}
+              icon={
+                isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+              }
+              variant={'ghost'}
+              aria-label={'Toggle Navigation'}
+            />
+          </Flex>
+          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+            <Text
+              textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+              fontFamily={'heading'}
+              color={useColorModeValue('gray.800', 'white')}>
+              <Img boxSize='38px' w='58px' h='auto' src={imageLogo} />
+            </Text>
+            <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+              <DesktopNav />
+            </Flex>
+          </Flex>
+
+          <Stack
+            flex={{ base: 1, md: 0 }}
+            justify={'flex-end'}
+            direction={'row'}
+            spacing={6}>
+            <Button
+              as={'a'}
+              fontSize={'sm'}
+              fontWeight={400}
+              variant={'link'}
+            >
+              <LinkRouter
+              style={{
+                color: '#718096'
+              }}
+              to='/register/user'
+              >
+                Cadastre-se
+              </LinkRouter>
+            </Button>
+            <Button
+              display={{ base: 'none', md: 'inline-flex' }}
+              fontSize={'sm'}
+              fontWeight={600}
+              color={'white'}
+              bg={'green.400'}
+              _hover={{
+                bg: 'green.300',
+              }}>
+              Entrar
+            </Button>
+          </Stack>
+        </Flex>
+
+        <Collapse in={isOpen} animateOpacity>
+          <MobileNav />
+        </Collapse>
+      </Box>
+      <Container maxW={'3xl'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+            Agende serviços <br />
+            <Text as={'span'} color={'green.400'}>
+              para seu pet
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, et esse consectetur molestias dolorem voluptatum asperiores consequuntur ad velit illum rerum laudantium doloremque quaerat modi! Sequi corrupti doloremque optio ea.
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+            <Button
+              type='button'
+              onClick={() => navigate('/register/user')}
+              colorScheme={'green'}
+              bg={'green.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'green.500',
+              }}>
+              Agende Agora
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+      <Box
+        bg={useColorModeValue('gray.50', 'gray.900')}
+        color={useColorModeValue('gray.700', 'gray.200')}>
+        <Container as={Stack} maxW={'6xl'} py={10}>
+          <SimpleGrid
+            templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr' }}
+            spacing={8}>
+            <Stack spacing={6}>
+              <Box>
+                <Img w='250px' h='auto' src={imageLogo} />
+              </Box>
+              <Text fontSize={'sm'}>
+                © 2022 Ifaro. Direitos reservados.
+              </Text>
+              <Stack direction={'row'} spacing={6}>
+                <SocialButton label={'Twitter'} href={'#'}>
+                  <FaTwitter />
+                </SocialButton>
+                <SocialButton label={'YouTube'} href={'#'}>
+                  <FaYoutube />
+                </SocialButton>
+                <SocialButton label={'Instagram'} href={'#'}>
+                  <FaInstagram />
+                </SocialButton>
+              </Stack>
+            </Stack>
+            <Stack align={'flex-start'}>
+              <ListHeader>Empresa</ListHeader>
+              <Link href={'#'}>Sobre</Link>
+              <Link href={'#'}>Blog</Link>
+              <Link href={'#'}>Contato</Link>
+              <Link href={'#'}>Preço</Link>
+              <Link href={'#'}>Depoimentos</Link>
+            </Stack>
+            <Stack align={'flex-start'} >
+              <ListHeader>Área funcionários</ListHeader>
+              <LinkRouter style={{
+                color: '#2d3748'
+              }} to='/register/doctor'>Veterinário</LinkRouter>
+              <LinkRouter to='/register/groomer' style={{
+                color: '#2d3748'
+              }} >Groomer</LinkRouter>
+            </Stack>
+          </SimpleGrid>
+        </Container>
+      </Box>
+    </>
+  );
 }
+
+const DesktopNav = () => {
+  const linkColor = useColorModeValue('gray.600', 'gray.200');
+  const linkHoverColor = useColorModeValue('gray.800', 'white');
+  const popoverContentBgColor = useColorModeValue('white', 'gray.800');
+
+  return (
+    <Stack direction={'row'} spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={'hover'} placement={'bottom-start'}>
+            <PopoverTrigger>
+              <Link
+                p={2}
+                href={navItem.href ?? '#'}
+                fontSize={'sm'}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: 'none',
+                  color: linkHoverColor,
+                }}>
+                {navItem.label}
+              </Link>
+            </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={'xl'}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={'xl'}
+                minW={'sm'}>
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
+  return (
+    <Link
+      href={href}
+      role={'group'}
+      display={'block'}
+      p={2}
+      rounded={'md'}
+      _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
+      <Stack direction={'row'} align={'center'}>
+        <Box>
+          <Text
+            transition={'all .3s ease'}
+            _groupHover={{ color: 'pink.400' }}
+            fontWeight={500}>
+            {label}
+          </Text>
+          <Text fontSize={'sm'}>{subLabel}</Text>
+        </Box>
+        <Flex
+          transition={'all .3s ease'}
+          transform={'translateX(-10px)'}
+          opacity={0}
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify={'flex-end'}
+          align={'center'}
+          flex={1}>
+          <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
+  );
+};
+
+const MobileNav = () => {
+  return (
+    <Stack
+      bg={useColorModeValue('white', 'gray.800')}
+      p={4}
+      display={{ md: 'none' }}>
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+};
+
+const MobileNavItem = ({ label, children, href }: NavItem) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Flex
+        py={2}
+        as={Link}
+        href={href ?? '#'}
+        justify={'space-between'}
+        align={'center'}
+        _hover={{
+          textDecoration: 'none',
+        }}>
+        <Text
+          fontWeight={600}
+          color={useColorModeValue('gray.600', 'gray.200')}>
+          {label}
+        </Text>
+        {children && (
+          <Icon
+            as={ChevronDownIcon}
+            transition={'all .25s ease-in-out'}
+            transform={isOpen ? 'rotate(180deg)' : ''}
+            w={6}
+            h={6}
+          />
+        )}
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.700')}
+          align={'start'}>
+          {children &&
+            children.map((child) => (
+              <Link key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Link>
+            ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  );
+};
+
+interface NavItem {
+  label: string;
+  subLabel?: string;
+  children?: Array<NavItem>;
+  href?: string;
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: 'Serviços',
+    children: [
+      {
+        label: 'Serviços disponíveis',
+        subLabel: 'Tosa, banho, aplicação de medicamentos, etc.',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Saúde',
+    children: [
+      {
+        label: 'Consultas com veterinário',
+        subLabel: 'Cuide da saúde do seu pet',
+        href: '#',
+      },
+    ],
+  },
+];
+
+const ListHeader = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: {
+  children: ReactNode;
+  label: string;
+  href: string;
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../theme'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateUser } from './components/CreateUser'
+import { CreateGroomer } from './components/CreateGroomer'
+import { CreateDoctor } from './components/CreateDoctor'
+import { Dashboard } from './components/Dashboard'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <BrowserRouter>
-      <App />
-      </BrowserRouter>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="/register/user" element={<CreateUser/>}/>
+      <Route path='/register/groomer' element={<CreateGroomer />} />
+      <Route path='/register/doctor' element={<CreateDoctor />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+    </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 )
