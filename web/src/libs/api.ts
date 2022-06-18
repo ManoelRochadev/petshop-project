c4ae8d07b4fb83770955445e031da23bb778f163
Manoel Rocha <manoelmelo367@gmail.com>
2022-06-18 22:02:53 +0000

    add link api local 2

--- web/src/libs/api.ts
+++ web/src/libs/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const url = 'http://localhost:4000' || 'https://petshop-project2-production.up.railway.app/'
+
 export const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: url,
 })
